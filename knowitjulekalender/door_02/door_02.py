7d7ce97f6a6050d4b0573becdc403738768be629
Cato Teig <kno-catei@fiskeridir.no>
2021-12-03 09:23:51 +0100

    door_02

--- knowitjulekalender/door_02/door_02.py
+++ knowitjulekalender/door_02/door_02.py
@@ -1,7 +1,7 @@
 import csv
 from haversine import haversine
 
-START_POSITION = (90, 135)
+START_POSITION = (90, 0)
 
 
 def csv_to_dict(csv_file):
@@ -17,13 +17,11 @@ def csv_to_dict(csv_file):
 
 def main():
 
-    curr_pos = START_POSITION
     city_list = csv_to_dict('cities.csv')
-    traveled = 0
+    curr_pos, traveled = START_POSITION, 0
 
     while city_list:
-        closest_distance = float('inf')
-        closest_city = None
+        closest_distance, closest_city = float('inf'), None
 
         for city in city_list:
             this_distance = haversine(curr_pos, city)
@@ -32,8 +30,8 @@ def main():
                 closest_city = city
 
         curr_pos = closest_city
-        city_list.pop(closest_city)
         traveled += closest_distance
+        city_list.pop(closest_city)
 
     traveled += haversine(curr_pos, START_POSITION)
 
