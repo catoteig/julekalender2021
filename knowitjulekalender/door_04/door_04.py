0916b9868aa85a23ebf2319e2eafe31eb8bcb75e
Cato Teig <kno-catei@fiskeridir.no>
2021-12-04 23:56:35 +0100

    door_04

--- knowitjulekalender/door_04/door_04.py
+++ knowitjulekalender/door_04/door_04.py
@@ -1,32 +1,22 @@
-from functools import cache
-
 TOTAL_STEPS = 1000079  # 666716,3333363
 TOTAL_STEPS_TEST = 11
 
-# Fikk tips om å først forsøke med færre nuller, fant da mønsteret og svar: 66666666666666666716,33333333333333333363
+# Fikk tips om å først forsøke med færre nuller, fant da mønsteret med å legge til 6-ere og 3-ere
+# og svaret: 66666666666666666716,33333333333333333363
 
 
 def main():
 
-    curr_pos, north = [0, 0], True
-    for _ in range(TOTAL_STEPS_TEST):
-        if north:
+    curr_pos, go_north = [0, 0], True
+    for _ in range(TOTAL_STEPS):
+        if go_north:
             curr_pos[1] += 1
-            north = False if mod(curr_pos[1], 3) and not mod(curr_pos[1], 5) else True
+            go_north = False if curr_pos[1] % 3 == 0 and curr_pos[1] % 5 != 0 else True
         else:
             curr_pos[0] += 1
-            north = True if mod(curr_pos[0], 5) and not mod(curr_pos[0], 3) else False
+            go_north = True if curr_pos[0] % 5 == 0 and curr_pos[0] % 3 != 0 else False
 
     print(curr_pos)
-
-
-@cache
-def mod(num, check):
-    res = 0
-    num = str(num)
-    for i in range(0, len(num)):
-        res = (res * 10 + int(num[i])) % check
-    return True if res == 0 else False
 
 
 if __name__ == '__main__':
