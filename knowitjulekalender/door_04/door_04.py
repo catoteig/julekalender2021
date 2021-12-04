c89b84c643417fc012e72ae652c545fdd7b7895c
Cato Teig <kno-catei@fiskeridir.no>
2021-12-04 23:50:16 +0100

    door_04

--- knowitjulekalender/door_04/door_04.py
+++ knowitjulekalender/door_04/door_04.py
@@ -1,22 +1,21 @@
 from functools import cache
 
-TOTAL_STEPS = 1000079
+TOTAL_STEPS = 1000079  # 666716,3333363
 TOTAL_STEPS_TEST = 11
+
+# Fikk tips om å først forsøke med færre nuller, fant da mønsteret og svar: 66666666666666666716,33333333333333333363
 
 
 def main():
 
     curr_pos, north = [0, 0], True
     for _ in range(TOTAL_STEPS_TEST):
-
         if north:
             curr_pos[1] += 1
-            if mod(curr_pos[1], 3) and not mod(curr_pos[1], 5):
-                north = False
+            north = False if mod(curr_pos[1], 3) and not mod(curr_pos[1], 5) else True
         else:
             curr_pos[0] += 1
-            if mod(curr_pos[0], 5) and not mod(curr_pos[0], 3):
-                north = True
+            north = True if mod(curr_pos[0], 5) and not mod(curr_pos[0], 3) else False
 
     print(curr_pos)
 
